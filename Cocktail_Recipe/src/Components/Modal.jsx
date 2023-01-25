7f09f6295b4e2b12f3b67d30c420dd9d5469bb82
WeiLong006 <lim.wei.long.1@gmail.com>
2023-01-25 14:23:02 +0800

    conditional for strIngredient completed

--- Cocktail_Recipe/src/Components/Modal.jsx
+++ Cocktail_Recipe/src/Components/Modal.jsx
@@ -3,6 +3,24 @@ import "../App.css";
 import Button from "./Button";
 
 const Modal = (props) => {
+  const ingredient = [];
+  const measurement = [];
+
+  for (let i = 1; i <= 15; i++) {
+    const defaultName = "strIngredient";
+    const name = defaultName + i;
+    if (props.drink[name]) {
+      console.log(props.drink[name]);
+    }
+  }
+
+  // const ingre = props.drink.map((item,id)=()=>{
+  //     return (
+  //         <div>
+
+  //         </div>
+  //     )
+  // })
   return (
     // Open Modal
     <div id="myModal" className="modal">
